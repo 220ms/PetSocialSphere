c8092c5c53db69013ec3c4f42218042cb302e88a
220ms <mjhicks0013@gmail.com>
2024-12-21 12:23:10 +1100

    Login/signup page

--- PetSocialSphere/src/App.tsx
+++ PetSocialSphere/src/App.tsx
@@ -1,19 +1,20 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import React from "react"
-import Contact from "./pages/Contact"
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import React from "react";
+import Login from "./pages/Login";
+import Contact from "./pages/Contact";
 function App() {
-
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ PetSocialSphere/src/components/Login.tsx
@@ -0,0 +1,12 @@
+import petsImg from "../assets/pets in park.png";
+const Login = () => {
+  return (
+    <div className="flex h-screen">
+      <div>
+        <img src={petsImg} alt="Pets in a park" />
+      </div>
+      <div>Login</div>
+    </div>
+  );
+};
+export default Login;
